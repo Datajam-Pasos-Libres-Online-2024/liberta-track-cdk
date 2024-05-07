3287c2cfae99f668af181f0f1ac98dd0afc9e3c3
Julián Andrés Méndez M. <j.mendezm@uniandes.edu.co>
2024-05-06 22:11:22 -0500

    [develop] Auth for consumers and enterprises

--- lib/liberta-track-cdk-stack.ts
+++ lib/liberta-track-cdk-stack.ts
@@ -1,70 +1,14 @@
 import * as cdk from 'aws-cdk-lib';
-import { CfnOutput, RemovalPolicy } from 'aws-cdk-lib';
-import { UserPool, AccountRecovery, VerificationEmailStyle, UserPoolClient, OAuthScope } from 'aws-cdk-lib/aws-cognito';
 import { Construct } from 'constructs';
+import { buildCognitoEnterpriseAuth } from './cognito-enterprise';
+import { buildCognitoConsumerAuth } from './cognito-consumer';
 
 export class LibertaTrackCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const cognito = new UserPool(this, "LibertaTrackUserPool", {
-      signInAliases: {
-        email: true,
-        username: false
-      },
-      selfSignUpEnabled: true,
-      standardAttributes: {
-        email: {
-          required: true
-        },
-        givenName: {
-          required: true,
-          mutable: true
-        },
-        familyName: {
-          required: true,
-          mutable: true
-        }
-      },
-      autoVerify: { email: true },
-      userVerification: {
-        emailSubject: 'You need to verify your email for the playground',
-        emailBody: 'Thanks for signing up Your verification code is {####}',
-        emailStyle: VerificationEmailStyle.CODE,
-      },
-      passwordPolicy: {
-        minLength: 8,
-        requireLowercase: true,
-        requireUppercase: true,
-        requireDigits: true
-      },
-      accountRecovery: AccountRecovery.EMAIL_ONLY,
-      removalPolicy: RemovalPolicy.DESTROY,
-    })
-
-    const userPoolClient = new UserPoolClient(this, "LibertaTrackClient", {
-      userPool: cognito,
-      authFlows: {
-        userPassword: true,
-        userSrp: true
-      },
-      generateSecret: false,
-      oAuth: {
-        flows: {
-          authorizationCodeGrant: true,
-        },
-        scopes: [OAuthScope.EMAIL, OAuthScope.OPENID, OAuthScope.PROFILE],
-        callbackUrls: ["http://localhost:3000"]
-      },
-    });
-
-    new CfnOutput(this, 'UserPoolId', {
-      value: cognito.userPoolId || "",
-    });
-
-    new CfnOutput(this, 'userPoolClientId', {
-      value: userPoolClient.userPoolClientId ||"",
-    })
+    const cognitoEnterprise = buildCognitoEnterpriseAuth(this)
+    const cognitoEConsumer = buildCognitoConsumerAuth(this)
 
   }
 }
